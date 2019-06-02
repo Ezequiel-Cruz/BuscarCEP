6b04527ce7c806c390ea0ef4521bdd40ff8aeedb
Ezequiel Teixeira <ezequielteixeiracr@gmail.com>
2019-06-02 15:18:30 -0300

    Buscar CEP versão 1.0

--- BuscarCep/BuscarCep/BuscarCep/MainPage.xaml.cs
+++ BuscarCep/BuscarCep/BuscarCep/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +13,34 @@ namespace BuscarCep
         public MainPage()
         {
             InitializeComponent();
+        }
+        private async void BtnBuscarCep_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtCep.Text))
+                    throw new InvalidOperationException("Informe o CEP");
+
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync($"http://viacep.com.br/ws/{txtCep.Text}/json/"))
+                    {
+                        if (!response.IsSuccessStatusCode)
+                            throw new InvalidOperationException("Algo aconteceu com a pesquisa de CEP");
+
+                        var result = await response.Content.ReadAsStringAsync();
+
+                        if (!string.IsNullOrWhiteSpace(result))
+                            await DisplayAlert("Cep Encontrado", result, "OK");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ah não!", ex.Message, "OK");
+            }
+
         }
     }
 }
